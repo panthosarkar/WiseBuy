a7e9af31979c5400cc48523e70587082f68faf82
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2024-06-22 22:04:33 +0600

    new update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navigation from "../src/Components/Navigation/Navigation";
+import Body from "./Components/Content-Body/Body";
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Body />
     </div>
   );
 }
---
+++ src/Components/Content-Body/Body.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Content-Body/Body.css";
+
+function Body() {
+  return (
+    <div className="body-container">
+      <div className="body-content">
+        <h1>
+          Shop smarter, <br />
+          Not harder
+        </h1>
+        <p>
+          WiseBuy,Shop smarter. Our platform empowers informed decisions with
+          price comparisons & personalized recommendations. Experience
+          intelligent shopping.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
